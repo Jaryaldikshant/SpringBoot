3193bfd3d79ff78cd40702aaab847bd54a7c33fc
Dikshant Jaryal <dikshant.jaryal08@gmail.com>
2025-08-25 15:57:50 +0530

    learn about hql

--- hibernate/src/main/java/com/dikshant/Main.java
+++ hibernate/src/main/java/com/dikshant/Main.java
@@ -3,9 +3,11 @@ package com.dikshant;
 import org.hibernate.Session;
 import org.hibernate.SessionFactory;
 import org.hibernate.Transaction;
+import org.hibernate.query.*;
 import org.hibernate.cfg.Configuration;
 
 import java.util.List;
+import java.util.Objects;
 
 public class Main {
     public static void main(String[] args) {
@@ -45,70 +47,79 @@ public class Main {
 //        sf.close();
 //
 //        System.out.println(student1);
+//
+//        Laptop l1 = new Laptop();
+//        l1.setLaptopId(4);
+//        l1.setBrand("Asus");
+//        l1.setModel("Strix");
+//        l1.setRam(32);
 
-        Laptop l1 = new Laptop();
-        l1.setLaptopId(1);
-        l1.setBrand("Asus");
-        l1.setModel("Rog");
-        l1.setRam(16);
-
-        Laptop l2 = new Laptop();
-        l2.setLaptopId(2);
-        l2.setBrand("HP");
-        l2.setModel("14s");
-        l2.setRam(8);
-
-        Laptop l3 = new Laptop();
-        l3.setLaptopId(3);
-        l3.setBrand("Lenovo");
-        l3.setModel("Yoga");
-        l3.setRam(10);
-
-        Progammers p1 = new Progammers();
-        p1.setpId(101);
-        p1.setpName("Max");
-        p1.setTech("Mechanical");
-
-        Progammers p2 = new Progammers();
-        p2.setpId(102);
-        p2.setpName("Rishu");
-        p2.setTech("Electric");
-
-
-        p1.setLaptops(List.of(l1,l2));
-        p2.setLaptops(List.of(l3));
+//        Laptop l2 = new Laptop();
+//        l2.setLaptopId(2);
+//        l2.setBrand("HP");
+//        l2.setModel("14s");
+//        l2.setRam(8);
+//
+//        Laptop l3 = new Laptop();
+//        l3.setLaptopId(3);
+//        l3.setBrand("Lenovo");
+//        l3.setModel("Yoga");
+//        l3.setRam(10);
+//
+//        Progammers p1 = new Progammers();
+//        p1.setpId(101);
+//        p1.setpName("Max");
+//        p1.setTech("Mechanical");
+//
+//        Progammers p2 = new Progammers();
+//        p2.setpId(102);
+//        p2.setpName("Rishu");
+//        p2.setTech("Electric");
+//
+//
+//        p1.setLaptops(List.of(l1,l2));
+//        p2.setLaptops(List.of(l3));
 
 
 
 
         SessionFactory sf = new Configuration()
-                .addAnnotatedClass(Progammers.class)
+//                .addAnnotatedClass(Progammers.class)
                 .addAnnotatedClass(Laptop.class)
                 .configure()
                 .buildSessionFactory();
 
         Session session = sf.openSession();
-        Transaction transaction = session.beginTransaction();
+//        Transaction transaction = session.beginTransaction();
+//        session.persist(l1);
 
-        session.persist(l1);
-        session.persist(l2);
-        session.persist(l3);
-
-        session.persist(p1);
-        session.persist(p2);
-
-        transaction.commit();
+//        session.persist(l2);
+//        session.persist(l3);
+//        session.persist(p1);
+//        session.persist(p2);
+//        transaction.commit();
 
 
-      session.close();
+        // select * from laptop where ram=32   ---> Sql
+        // from Laptop where ram=32 ----> Hql
 
-      Session session1 = sf.openSession();
+        String brand = "Asus";
 
-      Progammers p5 = session1.get(Progammers.class,102);
-//      System.out.println(p5);
+        Query query = session.createQuery("select brand,model from Laptop where brand like ?1");
+        query.setParameter(1,brand);
 
-      session1.close();
-      sf.close();
+          List<Object[]> laptops = query.getResultList();
+
+          for(Object[] data: laptops){
+              System.out.println((String) data[0] + " " +(String) data[1]);
+          }
+
+//        Laptop l1 = session.get(Laptop.class,3);
+        System.out.println(laptops);
+
+        session.close();
+
+        sf.close();
 
 
 
